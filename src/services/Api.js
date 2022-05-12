66ccbd30687245d6a04b17b12c0fce23cd4a1faa
Aimarest <amarcosestevez@gmail.com>
2022-05-12 13:33:23 +0200

    fetch

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,9 +1,9 @@
 //componente que contiene el fetch
 function dataApi(data) {
-  return fetch('https://awesome-profile-cards.herokuapp.com/card/', {
-    method: 'POST',
+  return fetch("//localhost:4000", {
+    method: "POST",
     body: JSON.stringify(data),
-    headers: { 'Content-Type': 'application/json' },
+    headers: { "Content-Type": "application/json" },
   }).then((response) => response.json());
 }
 
